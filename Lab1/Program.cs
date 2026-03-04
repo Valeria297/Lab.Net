c11100640f6bbe24b5f09185829f27710485b2d0
Valeria <valeriserbin2907@gmail.com>
2026-03-04 13:45:35 +0300

    Add Lab1: calculator app

---
+++ Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CalculatorExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Calc calculator = new Calc();
+            int summ = calculator.Add(x: 5, y: 11);
+
+            Console.WriteLine("5 + 11 is {0}.", summ);
+            Console.ReadLine();
+        }
+    }
+
+    class Calc
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
